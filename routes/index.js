56e711acff93f04f7ed1c5fbd1c890ffca8f9dfb
mingkaic <mingkaic@sfu.ca>
2015-05-21 22:49:14 -0700

    super simple intro page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var projects = [
+	{
+		url: "http://tinyurl.com/allegro-mkc",
+		description: "A simple single-page Point of Sales web-application using MEAN",
+		date_created: new Date(2015, 5, 10)
+	}];
+
+	var vm = {
+		title: 'Ming Kai Chen\'s Website',
+		intro: 'My name is Ming Kai Chen, and these are my Projects',
+		subtitle1: 'Websites',
+		list: projects
+	};
+	res.render('index', vm);
 });
 
 module.exports = router;
